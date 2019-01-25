d29ad5527ba020283407e210f327c03a13b462a0
pacmantz100 <45640468+pacmantz100@users.noreply.github.com>
2019-01-25 14:51:07 -0800

    Add files via upload

---
+++ neighborhood price.sql
@@ -0,0 +1,10 @@
+SELECT 
+	price,
+	neighbourhood,
+	room_type,
+	name
+FROM 
+	sfo_listings
+ORDER BY price DESC
+
+-- the most expensive home is a Full House Victorian in Western Addition and costs 10,000 for the entire Home/apt
